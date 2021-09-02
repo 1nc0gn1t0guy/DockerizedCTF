53798cc3d0424c83c62b64345b64b8c40c28c457
1nc0gn1t0guy <csr.dnl33@gmail.com>
2021-09-02 14:48:29 +0200

    mod database

--- sql/init.sql
+++ sql/init.sql
@@ -4,4 +4,4 @@ CREATE TABLE IF NOT EXISTS robots(nombre varchar(20), numero varchar (20));
 CREATE DATABASE IF NOT EXISTS users;
 USE users;
 CREATE TABLE IF NOT EXISTS users(username varchar(20), password varchar (20));
-INSERT INTO users(username, password) VALUES ('pepe', 'pepeadmin'), ('admin', 'R0b0t1cs!');
+INSERT INTO users(username, password) VALUES ('pepe', 'f25a2fc72690b780b2a14e140ef6a9e0'), ('admin', '5f4dcc3b5aa765d61d8327deb882cf99'); 
